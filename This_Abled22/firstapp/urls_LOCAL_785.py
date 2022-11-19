bf8bd730b90d0235164f8e051f49052a395f2d61
suxmxn <soomin1938@daum.net>
2022-11-20 02:43:34 +0900

    커뮤니티 각 게시판 생성

---
+++ This_Abled22/firstapp/urls_LOCAL_785.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.landing),
+    path('community_list/', views.community_list),
+    path('start_job_test/', views.start_job_test),
+    path('job_community/', views.job_community),
+    path('hobby_community/', views.hobby_community),
+    path('notice_community/', views.notice_community),
+    path('qna_community/', views.qna_community),
+    path('admin_community/', views.admin_community),
+    path('job_making/', views.job_making),
+    path('hobby_exercise/', views.hobby_exercise),
+]
---
+++ This_Abled22/firstapp/urls_REMOTE_785.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.landing),
+    path('community_list/', views.community_list),
+    path('job_community/', views.job_community),
+    path('hobby_community/', views.hobby_community),
+    path('notice_community/', views.notice_community),
+    path('qna_community/', views.qna_community),
+    path('admin_community/', views.admin_community),
+    path('start_job_test/', views.start_job_test),
+    path('job_test_1/', views.job_test_1),
+]
