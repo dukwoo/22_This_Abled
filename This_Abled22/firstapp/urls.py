bcbf1ba6594d7472c6702ee936364b11929d8a4d
suxmxn <soomin1938@daum.net>
2022-11-19 20:18:47 +0900

    커뮤니티 게시판 페이지 연결 및 디자인 수정

--- This_Abled22/firstapp/urls.py
+++ This_Abled22/firstapp/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('',views.landing),
     path('community_list/', views.community_list),
+    path('job_community/', views.job_community),
+    path('hobby_community/', views.hobby_community),
+    path('notice_community/', views.notice_community),
+    path('qna_community/', views.qna_community),
+    path('admin_community/', views.admin_community),
 ]
--- This_Abled22/firstapp/views.py
+++ This_Abled22/firstapp/views.py
@@ -14,3 +14,33 @@ def community_list(request):
         request,
         'firstapp/community_list.html'
     )
+
+def job_community(request):
+    return render(
+        request,
+        'firstapp/job_community.html'
+    )
+    
+def hobby_community(request):
+    return render(
+        request,
+        'firstapp/hobby_community.html'
+    )
+
+def notice_community(request):
+    return render(
+        request,
+        'firstapp/notice_community.html'
+    )
+
+def qna_community(request):
+    return render(
+        request,
+        'firstapp/qna_community.html'
+    )
+
+def admin_community(request):
+    return render(
+        request,
+        'firstapp/admin_community.html'
+    )
