a53a45092d84f71720dd93db329b28f66b809024
JungYoonshin <sju08227@naver.com>
2022-11-26 03:04:55 +0900

    .

--- This_Abled22/This_Abled22/settings.py
+++ This_Abled22/This_Abled22/settings.py
@@ -146,6 +146,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Activate Django-Heroku.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
